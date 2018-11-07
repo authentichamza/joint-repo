810bd4fe9e8e9b76df8af678ded40a463287c514
hamza4465989 <43203240+hamza4465989@users.noreply.github.com>
2018-11-07 13:12:46 +0000

    main cpp
    Main cpp for Box

--- BoxMain.cpp
+++ BoxMain.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   Box box1,box2(1,2,3);
   box1.set_box(4,4,4);
-  cout << "Box1:__________" << '\n';
+  cout << "Box1:__________" << endl;
   cout << "Length Of Box: "<<b1.get_length() << endl;
   cout << "Width Of Box:  "<<b1.get_width() << endl;
   cout << "Height Of Box: "<<b1.get_height() << endl;
