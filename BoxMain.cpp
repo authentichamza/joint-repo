84421d9244f558099ef2943f1b3886ec28890d47
hamza4465989 <43203240+hamza4465989@users.noreply.github.com>
2018-11-07 13:11:17 +0000

    Add files via upload

---
+++ BoxMain.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"Box.h"
+using namespace std;
+int main(int argc, char const *argv[]) {
+  Box box1,box2(1,2,3);
+  box1.set_box(4,4,4);
+  cout << "Box1:__________" << '\n';
+  cout << "Length Of Box: "<<b1.get_length() << endl;
+  cout << "Width Of Box:  "<<b1.get_width() << endl;
+  cout << "Height Of Box: "<<b1.get_height() << endl;
+  cout << "Box2:__________" << endl;
+  cout << "Length Of Box 2: "<<b2.get_length() << endl;
+  cout << "Width Of Box 2: "<<b2.get_width() << endl;
+  cout << "Height Of Box 2: "<<b2.get_height() << endl;
+  return 0;
+}
